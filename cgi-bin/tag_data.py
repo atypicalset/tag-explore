21b1ed8daa46072ebacf2134e504c813ba8f08e1
xlx <atypicalset@gmail.com>
2012-06-08 01:17:36 +1000

    getting synset data is working
    TODO: draw bar graph

--- cgi-bin/tag_data.py
+++ cgi-bin/tag_data.py
@@ -11,13 +11,14 @@ import cgitb
 cgitb.enable()
 
 db_file = "./taginfo.db"
+db_wordnet = "./wordnet_tag.db"
 
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
 # Get data from fields, support keyword query and range query
 # data format either "raw" or "percentile"
-df = form.getvalue('dataformat')
+df = form.getvalue('dataformat') if form.getvalue('dataformat') else 'tagf'
 qword = form.getvalue('q')
 xfrom = float(form.getvalue('xfrom')) if form.getvalue('xfrom') else []
 xto = float(form.getvalue('xto')) if form.getvalue('xto') else []
@@ -37,28 +38,52 @@ if DEBUG_TEST and xfrom and xto:
 	print "range: x [%0.3f, %0.3f] y[%0.3f, %0.3f]" % (xfrom, xto, yfrom, yto)
 
 
-conn = sqlite3.connect(db_file)
-cursor = conn.cursor()
-failed = False
-if not qword and not xfrom: # get all the data
-	cursor.execute('SELECT tag, count, infm FROM tag_score')
-elif qword:
-	cursor.execute('SELECT tag, count, infm FROM tag_score WHERE tag=?', [qword] )
-elif xfrom and xto and yfrom and yto:
-	cursor.execute('SELECT tag, count, infm FROM tag_score WHERE count>=? AND count<=? AND infm>=? AND infm<=?', 
-		(xfrom, xto, yfrom, yto))
-else:
-	failed = True
+if df == 'tagf':
+	conn = sqlite3.connect(db_file)
+	cursor = conn.cursor()
+	failed = False
+	if not qword and not xfrom: # get all the data
+		cursor.execute('SELECT tag, count, infm FROM tag_score')
+	elif qword:
+		cursor.execute('SELECT tag, count, infm FROM tag_score WHERE tag=?', [qword] )
+	elif xfrom and xto and yfrom and yto:
+		cursor.execute('SELECT tag, count, infm FROM tag_score WHERE count>=? AND count<=? AND infm>=? AND infm<=?', 
+			(xfrom, xto, yfrom, yto))
+	else:
+		failed = True
 
-if not failed:
-	result = map(lambda t: {'tag': t[0], 'count': t[1], 'infm': t[2]}, cursor)
-	if DEBUG_TEST:
-		print " %d data points found " % len(result)
-	print json.dumps(result, sort_keys=True, indent=4)
+	if not failed:
+		result = map(lambda t: {'tag': t[0], 'count': t[1], 'infm': t[2]}, cursor)
+		if DEBUG_TEST:
+			print " %d data points found " % len(result)
+		print json.dumps(result, sort_keys=True, indent=4)
+	else:
+		print "DO NOT know how to get data, QUIT!"
+
+	conn.close()
+
+elif df=="wn":
+	failed = False
+	conn = sqlite3.connect(db_wordnet)
+	cursor = conn.cursor()
+	stmt = "SELECT T.wnid, W.words, T.count, T.percentage FROM wn_tag T, wordnet W " + \
+		"WHERE T.tag=? AND W.wnid=T.wnid ORDER BY T.count DESC" 
+	if qword:
+		cursor.execute(stmt, (qword,) )
+	else:
+		failed = True
+
+	if not failed:
+		result = map(lambda t: {'wnid': t[0], 'words': t[1], 'count': t[2], 'prct': t[3]}, cursor)
+		if DEBUG_TEST:
+			print " %d data points found " % len(result)
+		print json.dumps(result, sort_keys=True, indent=4)
+	else:
+		print "DO NOT know how to get data, QUIT!"
+
+	conn.close()
 else:
 	print "DO NOT know how to get data, QUIT!"
-
-conn.close()
 
 
 
